6c22bce48d98bb604ca557c83dd051f12f51390b
Supriya-Ranjan <supriya.ranjan2001@gmail.com>
2025-06-26 23:15:56 +0530

    responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,23 @@ import About from "./components/About";
 import Project from "./components/Project";
 import Contact from "./components/Contact";
 import FAQ from "./common/coreComponent/FAQ";
-import { FAQ_LIST } from "./common/enums/core";
 import Footer from "./common/coreComponent/Footer";
+import { FAQ_LIST } from "./common/enums/core";
 
 function App() {
   return (
     <Router>
-      <div>
-        <NavBar />
-        <div className="mx-20 my-10">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/project" element={<Project />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-          <FAQ faqList={FAQ_LIST} />
-        </div>
-        <Footer />
+      <NavBar />
+      <div className="mx-20 my-10">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <FAQ faqList={FAQ_LIST} />
       </div>
+      <Footer />
     </Router>
   );
 }
